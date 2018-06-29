695052e3352ab7aff67ce9a423f17de5593c93ff
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2018-06-29 14:18:40 -0700

    help!

--- app.js
+++ app.js
@@ -190,6 +190,12 @@ checkIfWords("bonzibuddy","Y(^o^)Y https://www.youtube.com/watch?v=MiRaRy4Qq8g Y
         })}
   }
 
+  if (checkIfWords("!c")) {
+    if(checkIfWords("github")) {
+      message.channel.send("Here is the link to the Github Repo: https://github.com/kylerchin/chloe");
+    }
+  }
+
 //help
   if(command === "/help") {
 
