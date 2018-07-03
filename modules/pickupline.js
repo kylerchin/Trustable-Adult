b7c56ebd5eebfc7f54b72aeff0767134a43c0519
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2018-07-03 16:31:49 -0700

    add omlette pickup line

---
+++ modules/pickupline.js
@@ -0,0 +1,9 @@
+exports.list = [
+  "I’m making a documentary on rare unique and exotic things on earth. Can we set up a lunch this afternoon to talk about you?",
+  "Are you a pair of scissors? Because you look sharp ✂️",
+  "Do you know CPR? Because I bet you take everyone's breath away",
+  "You make my heart take flight✈️",
+  "You know what's on the menu? Me-n-u 😘 ",
+  "Are you a library book? Because I'm checking you out 📚",
+  "According to search results, the moon has less gravity than Earth, But I'd still fall for you there.",
+  "Omlette you in on a secret - life is feta when we're together"];
