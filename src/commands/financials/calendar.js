bff809403c76d9dfe6b041c6cb1aa3e98bf4458e
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2018-10-30 19:22:32 -0700

    Add Economic Calender

---
+++ src/commands/financials/calendar.js
@@ -0,0 +1,35 @@
+const Command = require('./../command');
+const { rand, prepare } = require('./../../helpers');
+const request = require('request');
+const cheerio = require('cheerio');
+
+  class Hit extends Command {
+
+    /**
+     * Initializes the new Command Instance.
+     */
+    constructor() {
+        super({
+            desc: 'Show top market indexes currently',
+            aliases: ['hit','HIT'],
+        });
+    }
+
+    /**
+     * Executes the Command logic.
+     *
+     * @param {Object} config
+     */
+    execute(config) {
+        super.execute(config);
+
+        const { msg, params } = config;
+        const { sector } = params;
+
+        const cheerio = require('cheerio');
+        const request = require('request');
+
+  }
+}
+
+module.exports = new Hit();
